40c45ec9399c42b962acb67f8bb48571ce79108c
mschrage <michael.schrage@fincon.eu>
2022-06-27 15:45:47 +0200

    Weiterer Service + Fehlerhandling
    Außerdem serverside streaming

--- src/main/java/net/schrage/server/BankService.java
+++ src/main/java/net/schrage/server/BankService.java
@@ -1,9 +1,8 @@
 package net.schrage.server;
 
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
-import net.schrage.models.Balance;
-import net.schrage.models.BalanceCheckRequest;
-import net.schrage.models.BankServiceGrpc;
+import net.schrage.models.*;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase{
 
@@ -17,5 +16,28 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase{
 
     responseObserver.onNext(balance);
     responseObserver.onCompleted();
+  }
+
+  @Override
+  public void withdraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
+    int accountNumber = request.getAccountNumber();
+    int amount = request.getAmount(); //10, 20, 30
+
+    int balance = AccountDatabase.getBalance(accountNumber);
+
+    if (balance < amount) {
+      Status status = Status.FAILED_PRECONDITION.withDescription("not enough money. You have only " + balance);
+      responseObserver.onError(status.asRuntimeException());
+      return;
+    }
+    // all the validations passed
+    for (int i = 0; i < (amount / 10); i++) {
+      Money money = Money.newBuilder().setValue(10).build();
+      responseObserver.onNext(money);
+      AccountDatabase.deductBalance(accountNumber, 10);
+    }
+
+    responseObserver.onCompleted();
+
   }
 }
