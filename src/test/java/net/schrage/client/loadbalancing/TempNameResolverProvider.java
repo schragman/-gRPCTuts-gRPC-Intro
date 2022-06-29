34c9911197f75526fbc1b2bc634d578d065bd2f8
mschrage <michael.schrage@fincon.eu>
2022-06-29 12:35:51 +0200

    Client side Loadbalancing

---
+++ src/test/java/net/schrage/client/loadbalancing/TempNameResolverProvider.java
@@ -0,0 +1,30 @@
+package net.schrage.client.loadbalancing;
+
+import io.grpc.NameResolver;
+import io.grpc.NameResolverProvider;
+
+import java.net.URI;
+
+public class TempNameResolverProvider extends NameResolverProvider {
+  @Override
+  protected boolean isAvailable() {
+    return true;
+  }
+
+  @Override
+  protected int priority() {
+    return 5;
+  }
+
+  @Override
+  public String getDefaultScheme() {
+    return "dns";
+  }
+
+  @Override
+  public NameResolver newNameResolver(URI targetUri, NameResolver.Args args) {
+    System.out.println("Looking for service : " + targetUri);
+
+    return new TempNameResolver(targetUri.toString());
+  }
+}
